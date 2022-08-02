1a73ff0c6501cb71bb9cdcaeb962891bb2cb5de9
masab-debug <masadpd99@gmail.com>
2022-08-02 05:35:28 -0500

    bugs resolved

--- SubdomainsScrapper.sh
+++ SubdomainsScrapper.sh
@@ -15,31 +15,41 @@ do
         read -p "Location to save files: " location
         echo "Your location and filename of saving subdomains file: " $location
 
-
+        echo "Starting Sublist3r"
         #sublist3r subdomains
         sublist3r -d $domain -o $location
 
+        echo "Starting Rapiddns"
         #rapiddns subdomains
         curl -s "https://rapiddns.io/subdomain/$domain?full=1#result" | grep -B 1 "<td><a" | sed 's/<td><a.*//g' | grep -Po "([a-z0-9][a-z0-9\-]{0,61}[a-z0-9]\.)+[a-z0-9][a-z0-9\-]*[a-z0-9]" | sort -u | sort -n | cut -d" " -f2- | grep "$domain" >> $location
 
+        echo "Starting Ominsint"
         #ominsint subdomains
         curl -s "https://sonar.omnisint.io/subdomains/$domain" | grep -Po "([a-z0-9][a-z0-9\-]{0,61}[a-z0-9]\.)+[a-z0-9][a-z0-9\-]*[a-z0-9]" | sort -u | grep "$domain" >> $location
 
+
+        echo "Starting Wayback Url For Subdomains"
         #wayback machine
         waybackurls $domain | grep -oE "[a-zA-Z0-9._-]+\.$domain" |  grep "$domain" | uniq >> $location
 
+
+        echo "Starting subfinder and httpx"
         #subfinder and httpx
         subfinder -d $domain --silent | grep -Po "([a-z0-9][a-z0-9\-]{0,61}[a-z0-9]\.)+[a-z0-9][a-z0-9\-]*[a-z0-9]" | uniq | grep "$domain" >> $location
 
+        echo "Starting amass"
         #amass subdomain
         amass enum -passive -d $domain | sort -u >> $location
 
+        echo "Starting assetfinder"
         #assetfinder subdomains
         assetfinder -subs-only $domain | sort -u >> $location
 
+        echo "Starting subevil"
         #subevil subdomains
         python3 /opt/Bug\ Bounty\ Scripts/SubEvil/SubEvil.py -d $domain | sort -u >> $location
 
+        echo "Starting crh.sh"
         #crh.sh subdomains
         echo "Examples: %.yahoo.com, %25.bf1.yahoo.com, %25.%25.%25.%25.%25.yahoo.com, %25internal%25.yahoo.com
                 {you can put %25 wildcard anywhere in search like %25api.yahoo.com}
@@ -48,26 +58,32 @@ do
         read -p "Use CRSH quries here: " crsh
         curl -s "https://crt.sh/?q=$crsh" | grep -Po "([a-z0-9][a-z0-9\-]{0,61}[a-z0-9]\.)+[a-z0-9][a-z0-9\-]*[a-z0-9]" | grep "$domain" | sort -u >> $location
 
+        echo "Starting dnsrecon"
         #dnsrecon subdomains
         dnsrecon -d $domain -a | grep -oE "[a-zA-Z0-9._-]+\.$domain" | uniq >> $location
 
+        echo "Starting gobuster"
         #gobuster vhost bruteforce
         gobuster vhost --useragent "google" --wordlist "/usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-110000.txt" --url https://$domain | grep -oE "[a-zA-Z0-9._-]+\.$domain" >> $location
 
 
         cat $location | sort | uniq | awk '{ print length, $0 }' | sort -n | cut -d" " -f2- >> sortedSubdomains.txt
 
+        echo "Pinging using httpx"
         #pinging using httpx
         cat $location | httpx-toolkit -silent -mc 200,302,301,403 | awk '{ print length, $0 }' | sort -n | cut -d" " -f2- >> aliveDomains.txt
 
-
         echo "Successfully Extracted All Subdomains"
+
+        echo "Starting WayBackUrl"
+        #wayback machine
+        waybackurls $domain | uniq | awk '{ print length, $0 }' | sort -n | cut -d" " -f2- >> waybackurls.txt
+
+        
     else
         echo "$d is not a Valid Domain [FQDN]"
     fi
 done
-
-
 
 #notes:
 #add further more tools on requirement basis
