02acd8634656c4e2ab9a79d0c4c26623f136066a
masab-debug <masadpd99@gmail.com>
2022-08-11 07:07:50 -0500

    ffuf

--- SubdomainsScrapper.sh
+++ SubdomainsScrapper.sh
@@ -53,29 +53,40 @@ do
         echo "Starting crh.sh (Domain)"
 
         #crh.sh subdomains
-        echo "Examples: yahoo.com"
-            
-        read -p "Use CRSH quries here: " crsh
-        curl -s "https://crt.sh/?q=$crsh" | grep -Po "([a-z0-9][a-z0-9\-]{0,61}[a-z0-9]\.)+[a-z0-9][a-z0-9\-]*[a-z0-9]" | grep "$domain" | sort -u >> $location
-
-        echo "Starting crh.sh (PowerPlay)"
-        #crh.sh subdomains
         echo "Examples: %.yahoo.com, %25.bf1.yahoo.com, %25.%25.%25.%25.%25.yahoo.com, %25internal%25.yahoo.com
-                {you can put %25 wildcard anywhere in search like %25api.yahoo.com}
-                {Put some more wildcard in crt.sh website}
-                "
-        read -p "Use CRSH quries here: " crsh
-        curl -s "https://crt.sh/?q=$crsh" | grep -Po "([a-z0-9][a-z0-9\-]{0,61}[a-z0-9]\.)+[a-z0-9][a-z0-9\-]*[a-z0-9]" | grep "$domain" | sort -u >> $location
+                    {you can put %25 wildcard anywhere in search like %25api.yahoo.com}
+                    {Put some more wildcard in crt.sh website}
+                    "
+            
+        while true;
+            do
+            read -p "Use CRSH quries here: " crsh
+            curl -s "https://crt.sh/?q=$crsh" | grep -Po "([a-z0-9][a-z0-9\-]{0,61}[a-z0-9]\.)+[a-z0-9][a-z0-9\-]*[a-z0-9]" | grep "$domain" | sort -u >> $location
+            
+            echo "Continue to write [yes], if not press Enter"
+            read -p "What to perform crsh query again?" cond
+            if [ "$cond" = "yes" ]; then
+                continue
+            else
+                break;
+            fi
+        done
 
         echo "Starting dnsrecon"
         #dnsrecon subdomains
         dnsrecon -d $domain -a | grep -oE "[a-zA-Z0-9._-]+\.$domain" | uniq >> $location
 
-        echo "Gobuster is out"
+        # echo "Gobuster is out"
         # echo "Starting gobuster"
         # #gobuster vhost bruteforce
         # gobuster vhost --useragent "google" --wordlist "/usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-110000.txt" --url https://$domain | grep -oE "[a-zA-Z0-9._-]+\.$domain" | httpx-toolkit -silent -mc 200,302,301,403  >> $domain-aliveDomains.txt
 
+        echo "FFUF is Starting"
+        echo "FFUF enumerating HOSTS"
+        sudo ffuf -H "Host: FUZZ" -H "User-Agent: cloudflair" -c -w "/usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-110000.txt" -u https://yahoo.com/ -mc 200,302,403,500,301 >> hosts-ffuf-$location
+        
+        echo "FFUF enumerating VHOSTS"
+        sudo ffuf -H "Host: FUZZ.$domain" -H "User-Agent: cloudflair" -c -w "/usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-110000.txt" -u https://$domain/ -v -mc 200,302,403,500 | grep -A 2 '| URL |' | grep -oE "[a-zA-Z0-9._-]+\.$domain" >> vhosts-ffuf-$location
 
         cat $location | sort | uniq | awk '{ print length, $0 }' | sort -n | cut -d" " -f2- >> $domain-sortedSubdomains.txt
 
