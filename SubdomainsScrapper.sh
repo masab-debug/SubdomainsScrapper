8c0b8826ee294ad7340a31e5602d55620859ec5e
masab-debug <masadpd99@gmail.com>
2022-08-13 03:26:08 -0500

    More Changes

--- SubdomainsScrapper.sh
+++ SubdomainsScrapper.sh
@@ -114,6 +114,22 @@ do
         echo "Separating files according to extension"
         cat $domain-aliveUrls.txt | egrep -i -E -o "\.{1}\w*$" | sort -su >> EndPointsExtension.txt
 
+        echo ""
+        read -p "Folder name to save output" gfp
+        mkdir $gfp
+
+        ls ~/.gf | sed 's/[.].*$//' >> $gfp/gf-patterns.txt
+
+        cd $gfp
+        while read LINE
+                    do 
+                        cat ../test.txt | gf $LINE >> $LINE-urls.txt
+        done < gf-patterns.txt
+
+        find . -size 0 -delete
+
+        cd ..
+
         echo "Making files according to extensions found"
         while read LINE
             do 
